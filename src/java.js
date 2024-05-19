4a9a09895742b102e4da73d10edbb2cac0b90fcc
xo1111 <162613468+xo1111@users.noreply.github.com>
2024-05-19 23:42:51 +0100

    injected more weather data

--- src/java.js
+++ src/java.js
@@ -2,8 +2,39 @@ function displayTemperatureChange(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city-name");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let speedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date (response.data.time * 1000);
+
+    descriptionElement.innerHTML = response.data.condition.description;
     cityElement.innerHTML = response.data.city;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    speedElement.innerHTML = `${response.data.wind.speed} km/h`;
+    timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML= Math.round (temperature);
+}
+
+function formatDate (date){
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+        "Sunday",  
+        "Monday", 
+        "Tuesday", 
+        "wednesday", 
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days [date.getDay()];
+
+    if (minutes < 10 ){
+        minutes = `0${minutes}`
+    }
+
+    return `${day} ${hours}:${minutes},`
 }
 
 
