3aaa30676c13db0107f0fed473867c1ea82f9f1f
xo1111 <162613468+xo1111@users.noreply.github.com>
2024-05-20 00:53:03 +0100

    added icon ID,  restyled in css, injected img source from API

--- src/java.js
+++ src/java.js
@@ -7,13 +7,18 @@ function displayTemperatureChange(response){
     let speedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date (response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
+
+    //iconElement.innerHTML = `<img src= "${response.data.condition.icon_url}" class="weather-app-icon"`
     descriptionElement.innerHTML = response.data.condition.description;
     cityElement.innerHTML = response.data.city;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     speedElement.innerHTML = `${response.data.wind.speed} km/h`;
     timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML= Math.round (temperature);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />
+`;
 }
 
 function formatDate (date){
